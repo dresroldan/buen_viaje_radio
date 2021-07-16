890ed4dedf5d42022e0cafe0b207f66bac53b2c3
Andres Roldan <dresroldan@gmail.com>
2021-07-15 19:16:20 -0500

    Header component

---
+++ src/components/header.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Navbar, Nav, Container, NavDropdown } from 'react-bootstrap';
+import Logo from '../images/BV-Logo.png';
+import './header.css';
+
+const Header = () => {
+  return (
+    <nav>
+      <Navbar
+        collapseOnSelect
+        expand="lg"
+        variant="dark"
+        className="navbar-custom"
+      >
+        <Container>
+          <Navbar.Brand href="/">
+            <img src={Logo} style={{ width: 100 }} />
+          </Navbar.Brand>
+          <Navbar.Toggle
+            aria-controls="responsive-navbar-nav"
+            className="navbar-toggler"
+          />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav>
+              <Nav.Link href="/about">about</Nav.Link>
+              <Nav.Link eventKey={2} href="/about">
+                photos
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </nav>
+  );
+};
+
+export default Header;
